6c9de1d860c2a688bdea001b8fffd3d809f80b58
RoXtra1 <skornyakovartem@mail.ru>
2024-02-06 11:45:37 +0300

    добавлена функция авторизации

--- main.py
+++ main.py
@@ -6,6 +6,7 @@ from configparser import ConfigParser
 
 
 def create_config():
+    """ Создание и чтение файла конфигурации """
     cfg = ConfigParser()
     config_file = "Configuration.ini"
     if not os.path.exists(config_file):
@@ -16,3 +17,14 @@ def create_config():
             cfg.write(config_file)
     cfg.read("Configuration.ini", encoding='utf-8')
     return cfg
+
+
+def authorization():
+    """ Авторизация в вк через токен """
+    try:
+        vk_session = vk_api.VkApi(token=TOKEN)
+        print('Авторизован!')
+        return vk_session.get_api()
+    except vk_api.AuthError as error_msg:
+        print(error_msg)
+    return None
