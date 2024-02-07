e353c5d73372d20fa694f644e3092c5029144e08
RoXtra1 <skornyakovartem@mail.ru>
2024-02-07 13:19:55 +0300

    добавлена функция работы с сылкой и создания папок под альбом

--- main.py
+++ main.py
@@ -28,3 +28,67 @@ def authorization():
     except vk_api.AuthError as error_msg:
         print(error_msg)
     return None
+
+
+def link_read(link: str, path: str):
+    """ Функция обработки ссылки и создания папок под альбом """
+    global VK   # для повторной авторизации
+    print(f'Обрабатываю ссылку {link}')
+    group_id = link.split('/')[-1].split('_')[0].replace('album', '')
+    album_id = link.split('/')[-1].split('_')[1]
+
+    try:
+        album = VK.photos.getAlbums(owner_id=group_id, album_ids=album_id)['items'][0]
+    except requests.exceptions.ConnectionError:  # после скачивания 3-4х альбомов апи отключается
+        print('Попытка переподключения...')
+        VK = authorization()  # повторная авторизация
+        album = VK.photos.getAlbums(owner_id=group_id, album_ids=album_id)['items'][0]
+
+    photos_count = album['size']
+
+    if not os.path.exists(path):
+        try:
+            os.mkdir(path)
+        except Exception as e:
+            print(e)
+
+    album_dir = f"{path}\\{album['title'].rstrip('.').replace('/', '-')}"  # в названии папок нельзя использовать . и /
+    if not os.path.exists(album_dir):
+        os.mkdir(album_dir)
+    print(f"Скачиваю {album['title']} ({photos_count} шт.)...")
+
+    # Подсчитываем, сколько раз нужно получать список фото, т.к. 1000 фото - максимум для одного запроса.
+    # Число получится не целое - округляем в большую сторону
+    for j in range(math.ceil(photos_count / 1000)):
+        try:
+            photos = VK.photos.get(owner_id=group_id, album_id=album_id, count=1000,
+                                   offset=j * 1000)['items']  # Получаем 1000 фото
+        except requests.exceptions.ConnectionError:  # на случай, если фото больше 1000
+            print('Переподключение...')
+            VK = authorization()  # повторная авторизация
+            photos = VK.photos.get(owner_id=group_id, album_id=album_id, count=1000,
+                                   offset=j * 1000)['items']
+        counter = 1 + j * 1000
+
+        for photo in photos:
+            biggest = 0
+            biggest_src = ''
+
+            if counter % 10 == 0 or counter == photos_count:
+                print(f'Загружаю {counter} из {photos_count}...')
+
+            for size in photo['sizes']:  # Получаем наибольшее разрешение
+                if size['width'] > biggest:
+                    biggest = size['width']
+                    biggest_src = size['url']  # сохраняем ссылку на фото с наибольшим разрешением
+            content = requests.get(biggest_src).content
+
+            try:
+                with open(f"{album_dir}\\{str(counter)}.jpg", "wb") as f:
+                    f.write(content)
+            except (FileNotFoundError, NotADirectoryError, OSError) as e:
+                print(f"Ошибка с файлом - {e}")
+                continue
+
+            counter += 1
+    print(f'Альбом полностью загружен в {path}!\n')
