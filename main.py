c971bf41eb4b819f7741680544d5f5862c80b1cc
RoXtra1 <skornyakovartem@mail.ru>
2024-02-08 10:05:33 +0300

    добавлена функция работы со списком из файла

--- main.py
+++ main.py
@@ -9,13 +9,16 @@ def create_config():
     """ Создание и чтение файла конфигурации """
     cfg = ConfigParser()
     config_file = "Configuration.ini"
+
     if not os.path.exists(config_file):
         cfg.add_section("Configuration")
         cfg.set("Configuration", "Token", "token")  # публичный токен-ключ для авторизации
         cfg.set("Configuration", "Root", "root")  # адрес корневой папки, куда требуется скачивать
+
         with open(config_file, "w") as config_file:
             cfg.write(config_file)
     cfg.read("Configuration.ini", encoding='utf-8')
+
     return cfg
 
 
@@ -24,9 +27,11 @@ def authorization():
     try:
         vk_session = vk_api.VkApi(token=TOKEN)
         print('Авторизован!')
+
         return vk_session.get_api()
     except vk_api.AuthError as error_msg:
         print(error_msg)
+
     return None
 
 
@@ -46,15 +51,10 @@ def link_read(link: str, path: str):
 
     photos_count = album['size']
 
-    if not os.path.exists(path):
-        try:
-            os.mkdir(path)
-        except Exception as e:
-            print(e)
-
     album_dir = f"{path}\\{album['title'].rstrip('.').replace('/', '-')}"  # в названии папок нельзя использовать . и /
     if not os.path.exists(album_dir):
         os.mkdir(album_dir)
+
     print(f"Скачиваю {album['title']} ({photos_count} шт.)...")
 
     # Подсчитываем, сколько раз нужно получать список фото, т.к. 1000 фото - максимум для одного запроса.
@@ -91,4 +91,36 @@ def link_read(link: str, path: str):
                 continue
 
             counter += 1
-    print(f'Альбом полностью загружен в {path}!\n')
+    print(f'Альбом полностью загружен в {path}\n')
+
+
+def file_work(file_name: str):
+    """ Функция считывания списка ссылок из файла """
+    with open(file_name, "r", encoding='utf-8') as f:  # открываем файл, сохраняем список строк из него
+        content = f.readlines()
+
+    res = []
+    start = 0
+    for i in range(len(content)):
+        if content[i] == '\n':
+            end = i  # start и end это границы разделов, которые объединятся в папки
+            res.append(content[start:end])
+
+            if end + 1 <= len(content):
+                start = end + 1
+        elif i == len(content) - 1:
+            res.append(content[start:])
+        content[i] = content[i].rstrip()  # очистка от знаков переноса строки
+
+    for dir in res:  # проходимся по каждому разделу
+        base_dir = ''
+        sub_dir = ''
+
+        for line in dir:  # затем по каждой линии в разделе
+            if line.startswith('http'):  # если ссылка
+                path = f'{ROOT}{base_dir}{sub_dir}'
+                link_read(line, path)
+            elif line.startswith('-'):  # если название подраздела
+                sub_dir = '\\' + line[1:]
+            else:  # если название раздела
+                base_dir = '\\' + line
